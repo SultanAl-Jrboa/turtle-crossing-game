a737a6c8fc3caffb1b19eb05b659a0706aca9005
Sultan Mohammed <sultanaljrboua@gmail.com>
2023-07-27 14:08:01 +0300

    Poor Turtle

---
+++ Arduino/sketch_oct26a/sketch_oct26a.ino
@@ -0,0 +1,17 @@
+int led = 13;
+int button = 4;
+
+void setup() {
+ pinMode(led, OUTPUT);
+ pinMode(button, INPUT);
+
+}
+
+void loop() {
+ if (digitalRead(button) == HIGH) {
+    digitalWrite(led, HIGH);
+ }
+ else {
+  digitalWrite(led, LOW);
+ }
+}
